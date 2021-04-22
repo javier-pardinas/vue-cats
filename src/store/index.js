59c8ccf41bf42fbea36f40402f59d055cfe66186
javier-pardinas <javierpb275@gmail.com>
2021-04-22 10:30:37 +0200

    added list of cats to the state of store

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cats: [
+      {"id": 1, "name": "Pepe", "description": "White"},
+      {"id": 2, "name": "Paco", "description": "Black"},
+      {"id": 3, "name": "María", "description": "Orange"},
+      {"id": 4, "name": "Teresa", "description": "Grey"},
+      {"id": 5, "name": "Luis", "description": "White"},
+      {"id": 6, "name": "William", "description": "Black"},
+      {"id": 7, "name": "Cecilia", "description": "Orange"},
+      {"id": 8, "name": "Alba", "description": "Grey"},
+    ]
   },
   mutations: {
   },
